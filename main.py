767d7c1ee0b3e8915d6cb53e8d8d4a498bd3922c
Alyson6776 <alyson6776@gmail.com>
2025-10-25 18:34:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from bot import run_bot
 from keep_alive import keep_alive
+from bot import run_bot
 import asyncio
 
-if __name__ == "__main__":
-    keep_alive()
-    asyncio.run(run_bot())
+keep_alive()
+asyncio.run(run_bot())
