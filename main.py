2ac557e3bf917733991e85d5706f78ee82656cb1
Alyson6776 <alyson6776@gmail.com>
2025-10-25 17:49:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import asyncio
-import keep_alive
+import threading
+from keep_alive import keep_alive
 from bot import run_bot
+import asyncio
 
-keep_alive.keep_alive()
+def run_flask():
+    keep_alive()
+
+threading.Thread(target=run_flask).start()
 asyncio.run(run_bot())
